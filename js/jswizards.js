623e005f795939a4a1d550a3389b11709c7ef72f
Samaa Medhat <samaa@aserver.com>
2011-02-10 15:08:27 +0200

    Implemented save and callback functionalities Reference: STCI-1

--- js/jswizards.js
+++ js/jswizards.js
@@ -2,69 +2,97 @@ function Form() {
 	
 	 this.createForm = function(){
 		$.floatbox({
-		        content: '<div id="tabs"><ul></ul></div>\
-		        <input type="submit" value="Save" onclick="save();"/>\
-		        <input type="button" name="cancel" value="Cancel" onclick="window.location=javascript:void(0);" class="close-floatbox"/>',
+		        content: '<div id="form"><ul></ul></div>\
+		        <input type="submit" value="Save" onClick="save()"/>\
+		        <input type="button" name="cancel" value="Cancel" onclick="closeFloatBox()" class="close-floatbox"/>',
 		        fade: true
 		    });
 	 }
 	
 	this.addTab = function (name, tabid){
-		$('#tabs > ul' )[0].innerHTML += "<li><a href='#"+ tabid +"'><span>" + name + "</span></a></li>";
+		$('#form > ul' )[0].innerHTML += "<li><a href='#"+ tabid +"'><span>" + name + "</span></a></li>";
 	}
 	
 	this.finalize = function(){
-	$("#tabs").tabs();		
+	$("#form").tabs();
 	}
 	
-	function addInput(tabid, type, name, text, value, validator, optional) {
+	function addInput(tabid, type, name, text, value, optional, callbackname) {
 		if (!optional) optional = true;
 		
 		var required = '';
 		if (optional == true) {
 			required = 'required';
 		}
-		if (typeof value == "undefined") {
-			$('#tabs')[0].innerHTML += '<div id=' + tabid + '><form name="input"  method="get">\
-		' + text + '<input type=' + type + ' id=' + name + 'size="25" class=' + required + ' minlength="2" /></div>';
+		var contents = '';
+		//contents += '<div id=' + tabid + '><form name="input"  method="get">\
+		//' + text + '<input type=' + type + ' id=' + name + ' size="25" class=' + required + ' minlength="2"';
+		
+		
+		contents += '<div id=' + tabid + '><form name="input"  method="get">\
+        ' + text + '<input type=' + type + ' id=' + name + ' size="25" class=' + required + ' minlength="2"';
+        if (value != null) {
+        	contents += ' value=' + value;
+        }
+        contents += ' /></div>';
+        //alert(contents);
+		$('#form')[0].innerHTML += contents;
+		if (callbackname != null){
+			$('input').change(function(){
+				//alert('calling callback');
+				processCallback(callbackname);
+			})
+		};
+		/**
+		if (callbackname != null) {
+			contents += ' onchange=processCallback(callbackname)';
 		}
-		else {
-			$('#tabs')[0].innerHTML += '<div id=' + tabid + '><form name="input"  method="get">\
-		' + text + '<input type=' + type + ' id=' + name + 'size="25" class=' + required + ' value=' + value + ' minlength="2" /></div>';
+		
+		if (value != null) {
+			contents += ' value=' + value;
 		}
-		return $('form :input').val();
-	}
+
+		contents +=  ' /></div>';
+		alert(contents);
+		$('#form').html += contents;
+		**/
+	};
 	
-	this.addText = function(tabid, name, text, value, validator, optional) {
-		addInput(tabid, 'text', name, text, value, validator, optional);
+	/**
+	 * GOOD:<div id=tab_main><form name="input"  method="get">        What is your name<input type=text id=txt_namesize="25" class=required minlength="2" /></div>
+	 * BAD: <div id=tab_main><form name="input"  method="get">		What is your name<input type=text id=txt_name size="25" class=required minlength="2" onchange=processCallback(callbackname) /></div>
+	 */
+	
+	
+	this.addText = function(tabid, name, text, value, optional, callbackname) {
+		addInput(tabid, 'text', name, text, value, optional, callbackname);
+		//console.log($('#'+name).val());
 	}
 
-	this.addPassword = function(tabid, name, text, value, optional) {
-		addInput(tabid, 'password', name, text, value, optional);
+	this.addPassword = function(tabid, name, text, value, optional, callbackname) {
+		addInput(tabid, 'password', name, text, value, optional, callbackname);
 	}
 	
-	this.addChoice = function(tabid, name, text, values, selectedValue, optional) {
+	this.addChoice = function(tabid, name, text, values, selectedValue, optional, callbackname) {
 		var choicestring = '';
 		choicestring += '<div id=' + tabid + '><form name="input" method="get">' + text + '</br>';
 		for (value in values) {
-			if (typeof selectedValue != "undefined"){
-				choicestring += '<input type="radio"  name=' + name + 'checked />' + values[value][0] + '</br>';
+			if (selectedValue != null){
+				choicestring += '<input type="radio" id=' + name + ' name=' + name + 'checked />' + values[value][0] + '</br>';
 			}
-			else choicestring += '<input type="radio"  name=' + name + '/>' + values[value][0] + '</br>';
+			else choicestring += '<input type="radio" id=' + name + ' name=' + name + '/>' + values[value][0] + '</br>';
 		}
-		$('#tabs')[0].innerHTML += choicestring + '</div>';
-
-		return $('input[name=' + name + ']:checked').val();
+		$('#form')[0].innerHTML += choicestring + '</div>';
 	}
 	
-	this.addChoiceMultiple = function(tabid, name, text, values, selectedValue, optional) {
+	this.addChoiceMultiple = function(tabid, name, text, values, selectedValue, optional, callbackname) {
 		var choicestring = '';
 		choicestring += '<div id=' + tabid + '><form name="input" method="get">' + text + '</br>';
 		for (value in values) {
 			choicestring += '<input type="checkbox"  name=' + values[value] + '/>' + values[value] + '</br>';
 		}
-		$('#tabs')[0].innerHTML += choicestring + '</div>';
-		return $('input[name=' + name + ']:checked').val();
+		$('#form')[0].innerHTML += choicestring + '</div>';
+		//return $('input[name=' + name + ']:checked').val();
 	}
 	
 	this.message = function(tabid, name, text, bold, multiline) {
@@ -73,16 +101,13 @@ function Form() {
 		if (bold == true) text = text.bold();
 		
 		if (multiline == true) {
-			$('#tabs')[0].innerHTML += '<div id=' + tabid + '><form name="input"  method="get">\
+			$('#form')[0].innerHTML += '<div id=' + tabid + '><form name="input"  method="get">\
 		<textarea id=' + name + ' TextMode="multiLine">' + text + '</textarea>';
 		}
 		else {
-			$('#tabs')[0].innerHTML += '<div id=' + tabid + '><form name="input"  method="get">\
+			$('#form')[0].innerHTML += '<div id=' + tabid + '><form name="input"  method="get">\
 			<div id=' + name + '>' + text + '</div>';
 		}
 	}
 	
-	function save() {
-		
-	}
 }
--- js/jswizards_client.js
+++ js/jswizards_client.js
@@ -1,5 +1,5 @@
-function callAppserver(customerGuid, wizardName, applicationserver_ip, success) {
-          jQuery.ajax({url: "http://"+applicationserver_ip+"/appserver/rest/wizard_engine/start?customerGuid="+customerGuid+"&wizardName="+wizardName,
+function callAppserver(wizardurl, success, methodName) {
+          jQuery.ajax({url: wizardurl,
             dataType: 'jsonp',
             success: success,
             jsonp: 'jsonp_callback',
@@ -8,23 +8,78 @@ function callAppserver(customerGuid, wizardName, applicationserver_ip, success) 
           });
     };
     
+function start(customerGuid, wizardName, applicationserverIp, success) {
+	this.applicationserverIp = applicationserverIp;
+	this.wizardName = wizardName;
+	url = "http://"+applicationserverIp+"/appserver/rest/wizard_engine/start?customerGuid="+customerGuid+"&wizardName="+wizardName;
+	//callAppserver(url, success, 'start');
+    jQuery.ajax({url: url,
+	    dataType: 'jsonp',
+	    success: success,
+	    jsonp: 'jsonp_callback',
+	    cache: true,
+	    error: function(data, error) { alert(data + error); }
+	});
+
+}
+
+function result(sessionId, resultData, applicationserverIp, success) {
+	url = "http://"+applicationserverIp+"/appserver/rest/wizard_engine/result?sessionId="+sessionId+"&result="+resultData;
+	//callAppserver(url, success, 'result');
+    jQuery.ajax({url: url,
+	    dataType: 'jsonp',
+	    success: success,
+	    jsonp: 'jsonp_callback',
+	    cache: true,
+	    error: function(data, error) { alert(data + error); }
+	});
+}
+
+function callback(wizardName, methodName, formData, sessionId, applicationserverIp){
+	url = "http://"+applicationserverIp+"/appserver/rest/wizard_engine/callback?SessionId="+sessionId+"&wizardName="+wizardName
+	+"&methodName="+methodName+"&formData="+formData;
+    jQuery.ajax({url: url,
+	    dataType: 'jsonp',
+	    success: success,
+	    jsonp: 'jsonp_callback',
+	    cache: true,
+	    error: function(data, error) { alert(data + error); }
+	});
+}
+
+function stop(sessionId, applicationserverIp) {
+	url = "http://" + applicationserverIp + "/appserver/rest/wizard_engine/stop?sessionId=" + sessionId;
+    jQuery.ajax({url: url,
+	    dataType: 'jsonp',
+	    jsonp: 'jsonp_callback',
+	    cache: true,
+	});
+}
+
 var success = function(data, status){
             if (!data){
                 alert("No data returned!!");
             }
             else {
-                alert("Data returned: " + data);
-                processData(data);
+            	processData(data);
             }
         }
         
 function processData(jsondata) {
-	var dataobj = JSON.parse(jsondata[1]);
+	if (jsondata[0] != '{') {
+			this.sessionId = jsondata[0];
+			this.dataobj = JSON.parse(jsondata[1]);
+	}
+	else {
+		this.dataobj = JSON.parse(jsondata);
+	}
+	this.tabs = this.dataobj['params']['tabs'];
 	console.log(dataobj);
-	
+	$form = $('#form');
+	$form.replaceWith($form.html());
+
 	form = new Form();
 	form.createForm();
-	var tabs = dataobj['params']['tabs'];
 	for (tabindex in tabs) {
 		form.addTab(tabs[tabindex]['text'], tabs[tabindex]['name']);
 		elements = tabs[tabindex]['elements'];
@@ -34,20 +89,32 @@ function processData(jsondata) {
 			elementname = element['name'];
 			elementtext = element['text'];
 			controltype = element['control'];
-			password = element['password'];
+			optional = element['optional']
+			if ('password' in element) password = element['password'];
+			else password = false;
+			if ('callback' in element) callbackname = element['callback'];
+			else callbackname = null;
+
 			if (controltype == 'text') {
+				if ('value' in element) value = element['value'];
+				else value = null;
+
 				if (password == false) {
-					form.addText(tabid, elementname, elementtext);
+					form.addText(tabid, elementname, elementtext, value, optional, callbackname);
 				}
 				else {
-					form.addPassword(tabid, elementname, elementtext);
+					form.addPassword(tabid, elementname, elementtext, value, optional, callbackname);
 				}
 			}
 			else if (controltype == 'option') {
-				form.addChoice(tabid, elementname, elementtext, element['values']);
+				if ('selectedValue' in element) value = element['selectedValue'];
+				else value = 0;
+				form.addChoice(tabid, elementname, elementtext, element['values'], value, optional, callbackname);
 			}
 			else if (controltype == 'optionmultiple') {
-				form.addChoiceMultiple(tabid, elementname, elementtext, element['values']);
+				if ('selectedValue' in element) value = element['selectedValue'];
+				else value = 0;
+				form.addChoiceMultiple(tabid, elementname, elementtext, element['values'], value, optional, callbackname);
 			}
 			else if (controltype == 'label') {
 				form.message(tabid, elementname, elementtext, element['bold'], element['multiline']);
@@ -58,5 +125,49 @@ function processData(jsondata) {
 		};
 	}
 	form.finalize();
-	
+}
+
+function processCallback(callbackname) {
+	resultdata = save(false);
+	callback(this.wizardName, callbackname, JSON.stringify(resultdata), this.sessionId, this.applicationserverIp)
+}
+
+function getActiveTab(){
+	var tabs = $('#form').tabs();
+	var selected = tabs.tabs('option', 'selected');
+	return this.tabs[selected]['name'];
+}
+
+function save(callresult) {
+	if (typeof callresult == 'undefined') callresult = true;
+	var resultdata = new Object();
+	resultdata['activeTab'] = getActiveTab();
+	resultdata['tabs'] = this.tabs;
+	tabs = resultdata['tabs'];
+	for (tabindex in tabs) {
+		elements = tabs[tabindex]['elements'];
+		tabid = tabs[tabindex]['name'];
+		for (elementindex in tabs[tabindex]['elements']) {
+			element = elements[elementindex];
+			id = element['name'];
+			console.log(id);
+			controltype = element['control'];
+			if (controltype == 'text' || controltype == 'password') {
+				element['value'] = $('#' + id).val();
+			}
+			else if (controltype == 'option' || controltype == 'optionmultiple') {
+				element['value'] = $('#' + id + ':checked').val();
+			}
+		}
+	}
+	//console.log(resultdata);
+	if (callresult == true){
+		result(this.sessionId, JSON.stringify(resultdata), this.applicationserverIp, success)
+	}
+	return resultdata;
+};
+
+function closeFloatBox(){
+	stop(this.sessionId, this.applicationserverIp);
+	window.location = "javascript:void(0)";
 }
